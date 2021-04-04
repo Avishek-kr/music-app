baa875a6c5e6209cc47bc4bdb297bcf761d11b6a
Avishek-kr <ak251997@gmail.com>
2021-04-04 22:19:35 +0530

    songs data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Player from "./components/Player";
 import Songs from "./components/Songs";
 import './styles/app.scss';
+import Data from './util'
 
 
 function App() {
